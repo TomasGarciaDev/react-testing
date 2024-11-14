bba78e8eca171a4c2e280fa8b304a13004579f70
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-14 12:02:00 +0100

    Grouping Tests

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correnctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correnctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  const name = "Tomas";
-  render(<Greet name={name} />);
-  const textElement = screen.getByText(`Hello ${name}`);
-  expect(textElement).toBeInTheDocument();
+  test("renders with a name", () => {
+    const name = "Tomas";
+    render(<Greet name={name} />);
+    const textElement = screen.getByText(`Hello ${name}`);
+    expect(textElement).toBeInTheDocument();
+  });
 });
