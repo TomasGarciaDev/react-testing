672710e3fe0d9d7912dfa07b81c90adb0c3258d6
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-15 12:03:13 +0100

    getByRole Options

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -4,8 +4,22 @@ import { Application } from "./Application";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", { name: /name/i });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", { name: /bio/i });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
