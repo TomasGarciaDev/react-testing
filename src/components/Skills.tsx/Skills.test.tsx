8a8f313417a2c549b0c688e2c9ef9570180216aa
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-19 13:54:06 +0100

    findBy

--- src/components/Skills.tsx/Skills.test.tsx
+++ src/components/Skills.tsx/Skills.test.tsx
@@ -29,4 +29,16 @@ describe("Skills", () => {
     });
     expect(startLearningButton).not.toBeInTheDocument();
   });
+
+  test("Start learning button is eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: /start learning/i,
+      },
+      { timeout: 2000 }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
--- src/components/Skills.tsx/Skills.tsx
+++ src/components/Skills.tsx/Skills.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SkillsPorps } from "./skills.types";
 
 export const Skills = (props: SkillsPorps) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { skills } = props;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoggedIn(true);
+    }, 1500);
+  }, []);
   return (
     <>
       <ul>
