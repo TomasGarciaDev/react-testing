5235c38fa262fe30661c8173f34c0afd03b3a3e0
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-19 14:01:51 +0100

    Debugging

--- src/components/Skills.tsx/Skills.test.tsx
+++ src/components/Skills.tsx/Skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, logRoles } from "@testing-library/react";
 import { Skills } from "./Skills";
 
 describe("Skills", () => {
@@ -31,14 +31,17 @@ describe("Skills", () => {
   });
 
   test("Start learning button is eventually displayed", async () => {
-    render(<Skills skills={skills} />);
+    const view = render(<Skills skills={skills} />);
+    logRoles(view.container);
+    // screen.debug();
     const startLearningButton = await screen.findByRole(
       "button",
       {
-        name: /start learning/i,
+        name: "Start Learning",
       },
       { timeout: 2000 }
     );
+    // screen.debug();
     expect(startLearningButton).toBeInTheDocument();
   });
 });
