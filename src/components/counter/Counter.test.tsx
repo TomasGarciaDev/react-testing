6f02852979607174a3ab8b1e37a5a0db1a5c7d80
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-20 15:05:59 +0100

    Keyboard Interactions

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -2,9 +2,11 @@ import { screen, render } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import Counter from "./Counter";
 
+beforeEach(() => {
+  render(<Counter />);
+});
 describe("Counter", () => {
   test("renders correctly", () => {
-    render(<Counter />);
     const countElement = screen.getByRole("heading");
     expect(countElement).toBeInTheDocument();
     const incrementButton = screen.getByRole("button", { name: "Increment" });
@@ -12,14 +14,14 @@ describe("Counter", () => {
   });
 
   test("renderts a count of 0", () => {
-    render(<Counter />);
+    // render(<Counter />);
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("0");
   });
 
   test("renderts a count of 1 after clicking the increment button", async () => {
     user.setup();
-    render(<Counter />);
+    // render(<Counter />);
     const incrementButton = screen.getByRole("button", { name: "Increment" });
     await user.click(incrementButton);
 
@@ -29,12 +31,38 @@ describe("Counter", () => {
 
   test("renderts a count of 2 after clicking the increment button twice", async () => {
     user.setup();
-    render(<Counter />);
+    // render(<Counter />);
     const incrementButton = screen.getByRole("button", { name: "Increment" });
     await user.click(incrementButton);
     await user.click(incrementButton);
 
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
+  });
+
+  test("renders a count of 10 after clicking the set button", async () => {
+    user.setup();
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", { name: "Set" });
+    await user.click(setButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("elements are focused in the right order", async () => {
+    user.setup();
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", { name: "Set" });
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
   });
 });
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,10 +2,24 @@ import React, { useState } from "react";
 
 function Counter() {
   const [counter, setCounter] = useState(0);
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <input
+        type='number'
+        name='amount'
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          setCounter(amount);
+        }}
+      >
+        Set
+      </button>
     </div>
   );
 }
