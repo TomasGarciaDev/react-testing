dbdbd5ca2ae26885f396ce61ec03d99d69853133
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-20 14:21:08 +0100

    Pointer Interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Skills } from "./components/Skills.tsx/Skills";
+import Counter from "./components/counter/Counter";
 
 function App() {
   const skills = ["HTML", "CSS", "JavaScript"];
   return (
     <div className='App'>
-      <Skills skills={skills} />
+      <Counter />
     </div>
   );
 }
